607b34e54f5cf574ca21adae74771febf0e256e7
Tony Michael <tee.jhay1@gmail.com>
2024-03-13 14:37:35 +0100

    feat: reuseable card component

---
+++ components/Card.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="w-[324px] p-7 px-8 h-[120px] flex flex-col justify-between rounded-lg"
+    :class="[variant === 'primary' ? 'bg-[#FCF1E8]' : 'bg-[#E8EEED]']">
+    <!-- card  title -->
+    <h2 class="uppercase text-[#71717A] text-sm ">
+      <slot name="title"></slot>
+    </h2>
+
+    <div class="flex items-end justify-between">
+      <!-- price -->
+      <h3 class="text-xl font-bold" :class="[variant === 'primary' ? 'text-[#DC6F19]' : 'text-[#165049]']">
+        <slot name="price"></slot>
+      </h3>
+
+      <!-- stats -->
+      <div class=" space-x-1 text-[#22C55E] ">
+        <!-- negative or positivw -->
+        <span>+</span>
+        <!-- stat number -->
+        <span>
+          <slot name="statNumber"></slot>
+        </span>
+        <!-- stat direction icon -->
+        <span>&#8593;</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts" >
+const props = defineProps({
+  variant: {
+    type: String,
+    default: 'primary'
+  },
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="p-20 flex gap-5">
+    <Card>
+      <template #title>Today's sales</template>
+      <template #price>$12,345</template>
+      <template #statNumber>36</template>
+    </Card>
+    <Card variant="secondary">
+      <template #title>Vendor</template>
+      <template #price>80</template>
+      <template #statNumber>36</template>
+    </Card>
+      <Card>
+        <template #title>Total Order</template>
+        <template #price>100</template>
+        <template #statNumber>36</template>
+      </Card>
+  </div>
+</template>
+<script>
+</script>
+
