d3c22fe3faabcaad2390c7d9d24d56288c8efe0f
Dev-Tonia <ogunwaanthonia@gmail.com>
2024-03-13 14:59:12 +0100

    feat/reusabale button

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+<template>
+  <BaseButton
+    :btnData="{
+      iconName: 'bi:filter',
+      iconColor: 'green',
+      title: 'Filter',
+    }"
+  />
+</template>
