0c9a9b4bfa45acb2d11e9ee60a338a35f10aab16
Dev-Tonia <ogunwaanthonia@gmail.com>
2024-03-14 08:20:53 +0100

    fix/takes into account other forms of button

--- components/BaseButton.vue
+++ components/BaseButton.vue
@@ -5,9 +5,20 @@ const { btnData } = defineProps(["btnData"]);
   <button
     class="px-4 py-2.5 bg-white border border-[#D0D5DD] rounded-lg shadow shadow-[#1018280D]"
   >
-    <div class="flex gap-2 items-center">
-      <Icon :name="btnData.iconName" :color="btnData.iconColor" size="30" />
+    <div
+      class="flex gap-2 items-center"
+      :class="{ 'justify-center': btnData.iconName === null }"
+    >
+      <Icon
+        v-if="btnData.iconName"
+        :name="btnData.iconName"
+        :color="btnData.iconColor"
+        size="30"
+      />
       <span> {{ btnData.title }} </span>
+      <!-- Slot for icon in the right -->
+
+      <slot />
     </div>
   </button>
 </template>
