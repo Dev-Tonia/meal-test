742bcf7d39a5e18df46d99dcc68b6e52848c88ef
Dev-Tonia <ogunwaanthonia@gmail.com>
2024-09-03 12:41:04 +0100

    fix: added the loading state and pagination

--- pages/dashboard/Customers.vue
+++ pages/dashboard/Customers.vue
@@ -14,6 +14,9 @@
         </CustomInput>
       </div>
     </div>
+    <Transition name="fade">
+      <Spinner v-if="pending" />
+    </Transition>
     <ReusableTable :table-titles="customerHeader">
       <TableRow v-for="(user, index) in usersData" :key="index">
         <TableCheckbox />
@@ -21,24 +24,52 @@
         <TableData :data="user.email" />
         <TableData :data="user.phone ? user.phone : 'N/A'" />
         <TableData
-          :data="user.delivery_address ? user.delivery_address : 'N/A'"
+          :data="user.delivery_address ? user.delivery_address.address : 'N/A'"
         />
         <TableData
           :data="user.orders_completed ? user.orders_completed : 'N/A'"
         />
       </TableRow>
     </ReusableTable>
+    <MTPagination
+      :total-pages="data.meta?.total"
+      :itemsPerPage="data.meta?.per_page"
+      @goto="gotoPage"
+      @prev-page="prevPage"
+      @next-page="nextPage"
+    />
   </section>
 </template>
 
 <script setup>
 import authHeader from "~/services/authHeader";
 
-const { data, pending, error } = await useApiCall("/admin/users/all", {
-  headers: authHeader(),
-});
+const pageNo = ref(1);
 
+const config = useRuntimeConfig();
+
+const { data, pending, error, refresh } = useFetch(
+  () =>
+    `${config.public.baseURL}/admin/users/all/customer?page=${pageNo.value}`,
+  {
+    headers: authHeader(),
+    key: `customer-${pageNo.value}`,
+  }
+);
 const usersData = computed(() => {
-  return data.value.data;
+  return data?.value.data;
 });
+const nextPage = (page) => {
+  pageNo.value = page;
+  refresh();
+};
+
+const gotoPage = (page) => {
+  pageNo.value = page;
+  refresh();
+};
+const prevPage = (page) => {
+  pageNo.value = page;
+  refresh();
+};
 </script>
