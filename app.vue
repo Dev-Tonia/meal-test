a5c539bb4c9f39c80efa34a3717ddcf7ac57dcce
Dev-Tonia <ogunwaanthonia@gmail.com>
2024-12-02 14:30:08 +0100

    fix: layout not displaying on the production

--- app.vue
+++ app.vue
@@ -1,4 +1,10 @@
 <template>
+  <p>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos soluta esse
+    corporis! Aliquid quae sapiente aspernatur, tenetur eius nesciunt illo
+    molestias odit possimus consectetur laudantium porro quaerat corporis
+    delectus unde.
+  </p>
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
